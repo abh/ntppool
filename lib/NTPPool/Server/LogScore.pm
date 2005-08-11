8129c9cb685d29bcf8b9603b7bb3bde2fd186adf
Ask Bjoern Hansen <ask@develooper.com>
2005-08-11 20:03:21 +0000

    add a few tests
    update sqlschema file
    
    
    git-svn-id: https://svn.develooper.com/projects/ntppool/trunk@128 e01dd7d5-6be1-0310-aa60-dbbdb055a346

--- lib/NTPPool/Server/LogScore.pm
+++ lib/NTPPool/Server/LogScore.pm
@@ -10,20 +10,28 @@ __PACKAGE__->has_a('server' => 'NTPPool::Server');
 
 sub history_symbol {
   my $self = shift;
-  my $step = $self->step;
-  if    ($step >= 0)  { return '#'; }
-  elsif ($step >= -1) { return 'x'; }
-  elsif ($step >= -4) { return 'o'; }
-  else { return '_'; }
+  _symbol($self->step);
 }
 
 sub history_css_class {
   my $self = shift;
-  my $step = $self->step;
+  _css_class($self->step);
+}
+
+sub _css_class {
+  my $step = shift;
   if    ($step >= 0)  { return 's_his s_his_ok'; }
   elsif ($step >= -1) { return 's_his s_his_tol'; }
   elsif ($step >= -4) { return 's_his s_his_big'; }
   else { return 's_his s_his_down'; }
+}
+
+sub _symbol {
+  my $step = shift;
+  if    ($step >= 0)  { return '#'; }
+  elsif ($step >= -1) { return 'x'; }
+  elsif ($step >= -4) { return 'o'; }
+  else { return '_'; }
 }
 
 
