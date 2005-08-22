cd30177cf9368635f4b58859b605f45d87ec50c7
Ask Bjoern Hansen <ask@develooper.com>
2005-08-22 21:38:07 +0000

    graph tweaks
    change to use UTC
    
    
    git-svn-id: https://svn.develooper.com/projects/ntppool/trunk@179 e01dd7d5-6be1-0310-aa60-dbbdb055a346

--- lib/NTPPool/DBI.pm
+++ lib/NTPPool/DBI.pm
@@ -8,7 +8,22 @@ use Class::DBI::Plugin::Pager;
 sub dbh { db_Main(@_) }
 
 sub db_Main {
-    my $dbh = db_open('ntppool', { shift->_default_attributes });
+    my $dbh;
+    if ( $ENV{'MOD_PERL'} and !$Apache::ServerStarting ) {
+        $dbh = Apache->request()->pnotes('dbh');
+    }
+    
+    unless ($dbh) {
+        $dbh = db_open('ntppool', { shift->_default_attributes });
+        unless ($dbh->{private_tz_set}) {
+            $dbh->do(q[set time_zone = '+00:00']);
+            $dbh->{private_tz_set} = 1;
+        }
+        __PACKAGE__->_remember_handle('Main');
+    }
+    if ( $ENV{'MOD_PERL'} and !$Apache::ServerStarting ) {
+        Apache->request()->pnotes('dbh', $dbh);
+    }
     $dbh;
 }
 
