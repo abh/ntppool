a04a2b132834109dcd72656215d59716068852bd
Ask Bjoern Hansen <ask@develooper.com>
2005-08-24 22:03:14 +0000

    server urls
    git-svn-id: https://svn.develooper.com/projects/ntppool/trunk@187 e01dd7d5-6be1-0310-aa60-dbbdb055a346

---
+++ lib/NTPPool/Server/URL.pm
@@ -0,0 +1,16 @@
+package NTPPool::Server::URL;
+use strict;
+use base qw(NTPPool::DBI);
+use Class::DBI::AbstractSearch;
+use Time::Piece;
+use Time::Piece::MySQL;
+
+__PACKAGE__->set_up_table('server_urls');
+__PACKAGE__->columns(Essential => __PACKAGE__->columns);
+
+__PACKAGE__->has_a('server' => 'NTPPool::Server');
+
+
+
+1;
+
