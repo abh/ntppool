30a06478347ddcacf894d7881d6b164437bebbba
Ask Bjoern Hansen <ask@develooper.com>
2005-11-04 05:30:33 +0000

    /s/$id shortcut for the server scores make the scores urls /scores/$ip with redirects from the other variations
    git-svn-id: https://svn.develooper.com/projects/ntppool/trunk@245 e01dd7d5-6be1-0310-aa60-dbbdb055a346

--- lib/NTPPool/Control/Scores.pm
+++ lib/NTPPool/Control/Scores.pm
@@ -6,15 +6,24 @@ use Apache::Constants qw(OK);
 
 sub render {
   my $self = shift;
-  my $ip = $self->req_param('ip');
-  if ($ip) {
-    my ($server) = NTPPool::Server->find_server($ip) or return 404;
-    $self->tpl_param('server' => $server);
+
+  if ($self->request->uri =~ m!^/s/(\d+)!) {
+    my ($server) = NTPPool::Server->find_server($1) or return 404;
+    return $self->redirect('/scores/' . $server->ip);
   }
 
+  if (my $ip = $self->req_param('ip')) {
+      my ($server) = NTPPool::Server->find_server($ip) or return 404;
+      return $self->redirect('/scores/' . $server->ip);
+  }
 
+  if ($self->request->uri =~ m!^/scores/(.*)!) {
+      my $p = $1;
+      my ($server) = NTPPool::Server->find_server($p);
+      return $self->redirect('/scores/' . $server->ip) unless $p eq $server->ip;
+      $self->tpl_param('server' => $server);
+  }
   return OK, $self->evaluate_template('tpl/server.html');
-
 }
 
 1;
