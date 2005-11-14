62c7420ce282ed8fe8661d48c82fef11052cd20b
Ask Bjoern Hansen <ask@develooper.com>
2005-11-14 20:53:57 +0000

    return 404 rather than 500 on /scores/unknown_server_name
    make /s/ redirect to /scores/
    
    
    git-svn-id: https://svn.develooper.com/projects/ntppool/trunk@255 e01dd7d5-6be1-0310-aa60-dbbdb055a346

--- lib/NTPPool/Control/Scores.pm
+++ lib/NTPPool/Control/Scores.pm
@@ -7,6 +7,8 @@ use Apache::Constants qw(OK);
 sub render {
   my $self = shift;
 
+  return $self->redirect('/scores/') if ($self->request->uri =~ m!^/s/$!);
+
   if ($self->request->uri =~ m!^/s/([^/]+)!) {
     my $server = NTPPool::Server->find_server($1) or return 404;
     return $self->redirect('/scores/' . $server->ip);
@@ -17,12 +19,15 @@ sub render {
       return $self->redirect('/scores/' . $server->ip) if $server;
   }
 
-  if ($self->request->uri =~ m!^/scores/(.*)!) {
+  if ($self->request->uri =~ m!^/scores/(.+)?!) {
       my $p = $1;
-      my ($server) = NTPPool::Server->find_server($p);
-      return $self->redirect('/scores/' . $server->ip) unless $p eq $server->ip;
-      return OK, $server->log_scores_csv(500), 'text/plain' if $self->req_param('log');
-      $self->tpl_param('server' => $server);
+      if ($p) {
+          my ($server) = NTPPool::Server->find_server($p);
+          return 404 unless $server;
+          return $self->redirect('/scores/' . $server->ip) unless $p eq $server->ip;
+          return OK, $server->log_scores_csv(500), 'text/plain' if $self->req_param('log');
+          $self->tpl_param('server' => $server);
+      }
   }
   return OK, $self->evaluate_template('tpl/server.html');
 }
