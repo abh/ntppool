7e7b5a7d8e8ddd18acca7c32868683e6de0bda99
Ask Bjoern Hansen <ask@develooper.com>
2006-01-16 11:40:57 +0000

    r2914@g5:  ask | 2006-01-16 03:40:33 -0800  make /s redirect to /scores/
     make the old .php graph urls work (at least the .html pages...)
    
    
    git-svn-id: https://svn.develooper.com/projects/ntppool/trunk@315 e01dd7d5-6be1-0310-aa60-dbbdb055a346

--- lib/NTPPool/Control/Scores.pm
+++ lib/NTPPool/Control/Scores.pm
@@ -7,14 +7,14 @@ use Apache::Constants qw(OK);
 sub render {
   my $self = shift;
 
-  return $self->redirect('/scores/') if ($self->request->uri =~ m!^/s/$!);
+  return $self->redirect('/scores/') if ($self->request->uri =~ m!^/s/?$!);
 
   if ($self->request->uri =~ m!^/s/([^/]+)!) {
     my $server = NTPPool::Server->find_server($1) or return 404;
     return $self->redirect('/scores/' . $server->ip);
   }
 
-  if (my $ip = $self->req_param('ip')) {
+  if (my $ip = ($self->req_param('ip') || $self->req_param('server_ip'))) {
       my $server = NTPPool::Server->find_server($ip) or return 404;
       return $self->redirect('/scores/' . $server->ip) if $server;
   }
