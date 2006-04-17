a5cc823d05ed21ad38c8d3bad9a2681bccc0c454
Ask Bjoern Hansen <ask@develooper.com>
2006-04-17 20:48:58 +0000

    r5956@g5:  ask | 2006-04-17 13:45:31 -0700  more vendor page
     simplify the "show graph" stuff a little
    
    
    git-svn-id: https://svn.develooper.com/projects/ntppool/trunk@363 e01dd7d5-6be1-0310-aa60-dbbdb055a346

--- docs/ntppool/js/ntppool_public.js
+++ docs/ntppool/js/ntppool_public.js
@@ -1,16 +1,6 @@
 
-function show_graph_explanation() {
-  var id = 'graph_explanation';
-  var div;
-//  if (document.getElementById) {
-     div = document.getElementById(id);
-//  }
-//  else {
-//    div = document.all[id];
-//     
-//  }
-  if (div) {
-    div.style.display = 'block';  /* or = 'block' */ 
-        alert(div.innerHTML);
-  }
+function toggleDisplay(divId) {
+  var div = document.getElementById(divId);
+  div.style.display = (div.style.display=="block" ? "none" : "block");
 }
+
