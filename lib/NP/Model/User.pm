159e62ee24dbd6fc7c251f6ec96cd1c7554c7a56
Ask Bjoern Hansen <ask@develooper.com>
2007-02-12 02:49:05 +0000

    bad_server_notifications
    git-svn-id: https://svn.develooper.com/projects/ntppool/trunk@440 e01dd7d5-6be1-0310-aa60-dbbdb055a346

--- lib/NP/Model/User.pm
+++ lib/NP/Model/User.pm
@@ -5,4 +5,33 @@ sub privileges {
     shift->user_privilege(@_);
 }
 
+package NP::Model::User::Manager;
+use strict;
+
+my $BAD_THRESHOLD = -20;
+
+sub admins_to_notify {
+    my $class = shift;
+    my $ids = NP::Model->dbh->selectcol_arrayref
+      (qq[SELECT DISTINCT u.id
+           FROM
+             servers s
+             JOIN users u ON(s.user_id=u.id)
+             LEFT JOIN server_alerts sa ON(sa.server_id=s.id)
+           WHERE
+             s.score_raw <= $BAD_THRESHOLD
+              AND s.in_pool = 1
+              AND (sa.last_email_time IS NULL
+                   OR (DATE_SUB(NOW(), INTERVAL 14 DAY) > sa.last_email_time
+                       AND (sa.last_score+10) >= s.score_raw
+                     ) 
+                  )
+          ORDER BY s.user_id
+        ]
+        );
+    return unless $ids and @$ids;
+    $class->get_users
+      (query => [ id => $ids ]);
+}
+
 1;
