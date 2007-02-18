cce7b08924bc7c0108d2f1b69193b6ceec712c86
Ask Bjoern Hansen <ask@develooper.com>
2007-02-18 07:40:34 +0000

    start a bundle for the pool system
    git-svn-id: https://svn.develooper.com/projects/ntppool/trunk@453 e01dd7d5-6be1-0310-aa60-dbbdb055a346

---
+++ lib/Bundle/NP.pm
@@ -0,0 +1,73 @@
+package Bundle::NP;
+
+$VERSION = "1.00";
+
+1;
+
+__END__
+
+=head1 CONTENTS
+
+Params::Validate
+DateTime::Locale
+Class::Singleton
+DateTime::TimeZone
+DateTime
+Clone
+Class::Factory::Util
+DateTime::Format::Strptime
+DateTime::Format::Builder
+DateTime::Format::MySQL
+DateTime::Format::Pg
+Sub::Uplevel    # For Test::Exception
+Test::Exception # For Object::Deadly
+Devel::StackTrace
+Devel::Symdump
+Object::Deadly  # For Carp::Clan tests
+Carp::Clan
+Bit::Vector::Overload # For Carp::Clan
+
+Sub::Install
+Params::Util
+Data::OptList
+Sub::Exporter
+SQL::ReservedWords
+
+DBI
+DBD::mysql
+
+Rose::Object
+Rose::DateTime
+Time::Clock
+Rose::DB
+Clone::PP
+Rose::DB::Object
+
+HTML::Prototype
+Text::CSV_XS
+
+JSON
+
+Digest::SHA1
+Math::BigInt
+Class::ErrorHandler
+Authen::Bitcard
+
+Email::Send
+
+Digest::HMAC_MD5
+Net::IP
+Net::DNS
+
+DateTime::Format::Duration
+
+Net::NTP
+
+Geo::IP
+
+Locale::Object
+
+
+
+
+
