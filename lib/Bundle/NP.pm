ac1b3307f08a73d05f5a48a6445300b75b80c630
Ask Bjoern Hansen <ask@develooper.com>
2007-02-19 12:15:47 +0000

    update Bundle
    git-svn-id: https://svn.develooper.com/projects/ntppool/trunk@467 e01dd7d5-6be1-0310-aa60-dbbdb055a346

--- lib/Bundle/NP.pm
+++ lib/Bundle/NP.pm
@@ -6,6 +6,19 @@ $VERSION = "1.00";
 
 __END__
 
+
+=head1 NAME
+
+Bundle::YP - Modules required to run the YPBot system
+
+=head1 SYNOPSIS
+
+    cd ~/src/ntppool/trunk
+    sudo /pkg/bin/perl -MCPAN -I`pwd`/combust/misc/Bundle-Combust/lib \
+         -I`pwd`/lib -e 'install "Bundle::NP"'
+
+You should install Bundle::Combust first
+
 =head1 CONTENTS
 
 Params::Validate
@@ -19,6 +32,7 @@ DateTime::Format::Strptime
 DateTime::Format::Builder
 DateTime::Format::MySQL
 DateTime::Format::Pg
+Test::Builder
 Sub::Uplevel    # For Test::Exception
 Test::Exception # For Object::Deadly
 Devel::StackTrace
@@ -43,7 +57,9 @@ Rose::DB
 Clone::PP
 Rose::DB::Object
 
+HTML::Tree      # for HTML::Prototype
 HTML::Prototype
+
 Text::CSV_XS
 
 JSON
@@ -53,7 +69,13 @@ Math::BigInt
 Class::ErrorHandler
 Authen::Bitcard
 
+Module::Pluggable
+Return::Value
+Email::Simple
+Email::Address
 Email::Send
+Email::Abstract
+Email::Date
 
 Digest::HMAC_MD5
 Net::IP
@@ -65,8 +87,10 @@ Net::NTP
 
 Geo::IP
 
+Test::Pod
 Locale::Object
 
+Time::Duration
 
 
 
