a67614a7bb453e4a51ab3f68bc2b2bd4d7e34e82
Ask Bjørn Hansen <ask@develooper.com>
2012-03-28 01:21:21 -0700

    Cleanup whitespace

--- docs/shared/static/js/ntppool.js
+++ docs/shared/static/js/ntppool.js
@@ -5,47 +5,43 @@ if (!NP) var NP = {};
 
 (function() {
 
-  "use strict";
+    "use strict";
 
-  NP.netspeed_updated = function(server_id, data, status) {
-          $('#netspeed_' + server_id ).fadeIn(400);
-          $('#netspeed_' + server_id ).html(data.netspeed);
-          $('#zones_' + server_id ).html(data.zones);
-  };
+    NP.netspeed_updated = function(server_id, data) {
+        $('#netspeed_' + server_id ).fadeIn(400);
+        $('#netspeed_' + server_id ).html(data.netspeed);
+        $('#zones_' + server_id ).html(data.zones);
+    };
 
-  NP.update_netspeed = function(server_id, netspeed) {
+    NP.update_netspeed = function(server_id, netspeed) {
+        var pars = { "netspeed": netspeed, "server": server_id };
+        $('#netspeed_' + server_id ).fadeOut(50);
+        jQuery.getJSON( '/manage/server/update/netspeed', pars,
+            function(data, textStatus) {
+                NP.netspeed_updated(server_id, data);
+            }
+        );
+    };
 
-     var pars = { "netspeed": netspeed, "server": server_id };
+    $(document).ready(function () {
 
-     $('#netspeed_' + server_id ).fadeOut(50);
+        $("#busy").ajaxStart(function(){
+            $(this).show(70);
+        })
+        .ajaxStop(function(){
+            $(this).hide(70);
+        });
 
-     jQuery.getJSON( '/manage/server/update/netspeed', pars,
-                     function(data, textStatus) {
-                         NP.netspeed_updated(server_id, data);
-                     }
+        $("#profile_link a.profile_link_change")
+            .live('click',
+                function(event) {
+                    event.preventDefault();
+                    $.post(this.href, {},
+                        function(response) {
+                            $('#profile_link').html(response);
+                        }
                     );
-  };
-
-
-$(document).ready(function () {
-
-  $("#busy")
-    .ajaxStart(function(){
-        $(this).show(70);
-    })
-    .ajaxStop(function(){
-        $(this).hide(70);
+                }
+            );
     });
-
-  $("#profile_link a.profile_link_change")
-     .live('click',
-           function(event) {
-               event.preventDefault();
-               $.post(this.href, {},
-                      function(response) {
-                          $('#profile_link').html(response);
-                      });
-           }
-          );
-});
 }());
