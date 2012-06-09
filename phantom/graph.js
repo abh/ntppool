dbe1eee862153c01bcfe430d834c97cb24b27440
Ask Bjørn Hansen <ask@develooper.com>
2012-06-08 17:25:43 -0700

    Try (and fail) making Typekit with with PhantomJS

--- phantom/graph.js
+++ phantom/graph.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var page = require('webpage').create(),
     system = require('system'),
     address, output;
@@ -8,7 +10,16 @@ if (system.args.length < 3 || system.args.length > 4) {
 } else {
     address = system.args[1];
     output = system.args[2];
+    console.log("Fetching", address, "for", output);
     page.viewportSize = { width: 501, height: 233 };
+    page.onConsoleMessage = function (msg) {
+        console.log('Page msg:', msg);
+    };
+    page.customHeaders = {
+        // "Referer": "http://www.pool.ntp.org/?graphs"
+    };
+    page.settings.userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_4) AppleWebKit/534.57.2 (KHTML, like Gecko) Version/5.1.7 Safari/534.57.2";
+
     page.open(address, function (status) {
         if (status !== 'success') {
             console.log('Unable to load the address!');
