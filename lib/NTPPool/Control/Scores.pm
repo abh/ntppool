0334b00c3bb69096eaff2766027ebe773b6e1dc4
Ask Bjørn Hansen <ask@develooper.com>
2012-08-30 16:31:54 -0700

    Remove obsolete sparkline graph

--- lib/NTPPool/Control/Scores.pm
+++ lib/NTPPool/Control/Scores.pm
@@ -3,14 +3,8 @@ use strict;
 use base qw(NTPPool::Control);
 use Combust::Constant qw(OK DECLINED);
 use NP::Model;
-use Imager ();
 use List::Util qw(min);
 use JSON qw(encode_json);
-
-BEGIN {
-  die "Imager module needs to be compiled with png support"
-      unless grep { $_ eq 'png' } Imager->write_types;
-}
 
 sub render {
   my $self = shift;
@@ -118,9 +112,6 @@ sub render {
             my ($type) = ($self->request->uri =~ m{/(offset|score)\.png$});
             return $self->redirect( $server->graph_uri( $type ), 301);
           }
-          elsif ($mode eq 'spark') {
-              return OK, $self->history_sparkline_png($server), 'image/png';
-          }
           elsif ($mode eq '') {
               $self->tpl_param('graph_explanation' => 1) if $self->req_param('graph_explanation');
               $self->tpl_param('server' => $server);
@@ -136,85 +127,6 @@ sub render {
   }
 
   return OK, $self->evaluate_template('tpl/server.html');
-}
-
-sub history_sparkline_png {
-    my ($self, $server) = @_;
-
-    $self->cache_control('s-maxage=5400');
-
-    my $width  = 160;
-    my $height = 64;
-
-    my $history = $server->history(80);
-    my @d;
-    
-    # @d = (0,1,2.1,3.2,4.4,5.5,7,8.5,10,11.5,13,15,16,11,6,1,-4,-3,-2,-1);
-
-    @$history = reverse @$history;
-
-    my $score_count = @$history;
-
-    my $min = min(-5,map { $_->score } @$history);
-
-    $score_count ||= 1;
-
-    my $x_scale = $width / $score_count;
-    my $left_x = 0 + ($width - $score_count * $x_scale);
-
-    my $y_scale = $height / (20 - $min);
-
-    my $img = Imager->new(xsize => $width, ysize => $height);
-    $img->box(color => [255,255,255], filled => 1);
-
-    $img->line(color => [192,192,192],
-               x1 => 0,
-               x2 => $width,
-               y1 => 15 * $y_scale,
-               y2 => 15 * $y_scale, 
-               aa => 1,
-               endp => 0, 
-              );
-
-    my $left_h = shift @$history;
-    my $left_y = $y_scale * ($left_h ? _calc_y($left_h) : 1);
-
-    for my $h (@$history) {
-
-        my $y = $y_scale * _calc_y($h);
-        my $x = $left_x + $x_scale;
-
-        my $colors = [ [0,180,0], 'green' ];
-        $colors = [ [0, 128,128], 'yellow' ] if $h->step < .5;
-        $colors = [ [180, 0,0 ], 'red' ]     if $h->step < -0.5;
-
-        $img->line(color => $colors->[0],
-                   x1 => $left_x,
-                   x2 => $x,
-                   y1 => $left_y,
-                   y2 => $y, 
-                   aa => 1,
-                   endp => 0 );
-
-        $img->setpixel(x => $x, y => $y, color => $colors->[1]);
-
-        $left_h = $h;
-        $left_x = $x;
-        $left_y = $y;
-    }
-
-    my $output;
-    $img->scale(scalefactor => .25)->
-          filter(type=>'autolevels', usat => 0.001, lsat => 0.001)->
-          write(data => \$output, type => 'png');
-
-    return $output;
-
-}
-
-sub _calc_y {
-    my $h = shift;
-    (($h->score - 20) * -1) + 1;
 }
 
 sub bc_user_class { NP::Model->user }
