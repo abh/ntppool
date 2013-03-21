9b0fa9538e2121b9c0c53febbe131653691bdabf
Ask Bjørn Hansen <ask@develooper.com>
2013-03-20 22:58:52 -0700

    Remove feedburner

--- lib/NTPPool/Control/Basic.pm
+++ lib/NTPPool/Control/Basic.pm
@@ -14,6 +14,10 @@ sub render {
         return $self->redirect( $self->manage_url( $self->request->path ));
     }
 
+    if ($self->request->path eq '/ntppool') {
+        return $self->redirect("http://news.ntppool.org/atom.xml");
+    }
+
     if ($self->request->uri =~ m!^/robots.txt$!) {
         $self->force_template_processing(1)
     }
