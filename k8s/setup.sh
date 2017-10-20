51cc56a068e821a368853eced902687f6a531412
Ask Bjørn Hansen <ask@develooper.com>
2017-10-20 09:21:11 +0200

    Add quotas to gearmand deployment

--- k8s/setup.sh
+++ k8s/setup.sh
@@ -2,11 +2,15 @@
 kubectl create ns ntpbeta
 KUBE="kubectl -n ntpbeta"
 
+$KUBE rollout pause deploy ntppool
+
 $KUBE apply -f ntppool-quay-secrets.yaml
 
 for f in ??-*.yaml; do
   $KUBE apply -f $f
 done
+
+$KUBE rollout resume deploy ntppool
 
 sh setup-ingress.sh
 
