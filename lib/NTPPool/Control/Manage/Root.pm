98171a6339986621b26be9c063d0c30271268172
Ask Bjørn Hansen <ask@develooper.com>
2019-07-01 22:08:19 -0700

    wip: improve deletion ui, manage homepage

--- lib/NTPPool/Control/Manage/Root.pm
+++ lib/NTPPool/Control/Manage/Root.pm
@@ -1,14 +1,15 @@
 package NTPPool::Control::Manage::Root;
 use strict;
-use base qw(NTPPool::Control::Manage);
-use Combust::Constant qw(OK NOT_FOUND);
+# Be a simpler "Basic" controller than the main website one
+# to let manage be it's own templated site
+use base qw(NTPPool::Control NTPPool::Control::Manage Combust::Control::Basic);
 
 sub render {
     my $self = shift;
-    if ($self->request->path eq '/') {
-        return $self->redirect('/manage');
-    }
-    return NOT_FOUND;
+    #if ($self->request->path eq '/') {
+    #    return $self->redirect('/manage');
+    #}
+    return $self->SUPER::render(@_);
 }
 
 1;
