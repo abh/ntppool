7eaefee8d09f58894bcfc7008034f593bbc617eb
Ask Bjørn Hansen <ask@develooper.com>
2019-08-28 19:24:28 -0700

    Account creation cleanup, auth_token, etc

--- lib/NTPPool/Control/Manage/Account.pm
+++ lib/NTPPool/Control/Manage/Account.pm
@@ -8,42 +8,53 @@ use Combust::Constant qw(OK NOT_FOUND);
 sub manage_dispatch {
     my $self = shift;
 
+    my $account;
+    if (($self->req_param('a') || '') eq 'new') {
+        return 403 unless $self->check_auth_token;
+        $account = NP::Model->account->create(users => [ $self->user ]);
+    }
+
+    $account = $self->current_account unless $account;
+
+    unless ($account) {
+        # TODO: check for invitations and show "accept invitations screen"...
+        $account = NP::Model->account->create(users => [ $self->user ]);
+        $account->name($self->user->name);
+        NP::Model::Log->log_changes($self->user, "account", "account created", $account);
+        $account->save();
+    }
+
+    # check access
+    return $self->redirect("/manage/")
+        unless (
+            $account->id == 0
+            or $account->can_edit($self->user)
+        );
+
     if ($self->request->uri =~ m!^/manage/account$!) {
-        my $account;
-        if (($self->req_param('a') || '') eq 'new') {
-            warn "set new";
-            $account = NP::Model->account->create(users => [ $self->user ]);
-        }
-        $account = $self->current_account unless $account;
-        unless ($account) {
-            # TODO: check for invitations and show "accept invitations screen"...
-            $account = NP::Model->account->create(users => [ $self->user ]);
-            $account->name($self->user->name);
-            NP::Model::Log->log_changes($self->user, "account", "account created", $account);
-            $account->save();
-        }
-        return $self->render_edit if ($self->request->method eq 'post');
-        return $self->render_account($account);
+        return $self->render_account_edit
+            if ($self->request->method eq 'post' and !$self->req_param('new_form'));
+        return $self->render_account_form($account);
+    } elsif ($self->request->uri =~ m!^/manage/account/users$!) {
+        return $self->render_users($account);
     }
 
     return NOT_FOUND;
 }
 
-sub render_account {
+sub render_users {
     my ($self, $account) = @_;
-    return 404 unless $account;
-    return $self->redirect("/manage/") unless $account and $account->can_edit($self->user);
-    return $self->_render_form($account);
+    return $self->_render_users($account);
 }
 
-sub _render_form {
-    my $self    = shift;
-    my $account = shift;
+
+sub render_account_form {
+    my ($self, $account) = @_;
     $self->tpl_param('account', $account);
     return OK, $self->evaluate_template('tpl/account/form.html');
 }
 
-sub render_edit {
+sub render_account_edit {
     my $self = shift;
 
     my $account_token = $self->req_param('a');
@@ -64,6 +75,7 @@ sub render_edit {
         my $v = $self->req_param($f) || '';
         $v =~ s/^\s+//;
         $v =~ s/\s+$//;
+        $v = undef if ($f eq 'url_slug' and $v eq '');
         $account->$f($v);
     }
     $account->public_profile($self->req_param('public_profile') ? 1 : 0);
@@ -71,14 +83,14 @@ sub render_edit {
     unless ($account->validate) {
         my $errors = $account->validation_errors;
         $self->tpl_param('errors', $errors);
-        return $self->_render_form($account);
+        return $self->render_account_form($account);
     }
 
     $account->save;
 
     NP::Model::Log->log_changes($self->user, "account", "update account", $account, $old);
 
-    return $self->render_account($account);
+    return $self->render_account_form($account);
 }
 
 
