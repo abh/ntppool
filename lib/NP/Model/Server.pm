fe9513e80ab1a6355ccaa41819c2583746ac9f31
Ask Bjørn Hansen <ask@develooper.com>
2020-05-20 00:23:52 -0700

    Limit monitoring 'dead' servers to once every 65 minutes per monitor

--- lib/NP/Model/Server.pm
+++ lib/NP/Model/Server.pm
@@ -293,7 +293,12 @@ sub get_check_due {
          WHERE
            monitor_id = ?
            AND s.ip_version = ?
-           AND (ss.score_ts IS NULL or ss.score_ts < DATE_SUB( NOW(), INTERVAL ? minute) )
+           AND (ss.score_ts IS NULL
+                 OR (ss.score_raw > -90
+                       AND ss.score_ts < DATE_SUB( NOW(), INTERVAL ? minute)
+                     OR (ss.score_ts < DATE_SUB( NOW(), INTERVAL 65 minute))
+                 )
+               )
            AND (s.score_ts IS NULL or s.score_ts < DATE_SUB( NOW(), INTERVAL ? minute) )
            AND (deletion_on IS NULL or deletion_on > NOW())
          ORDER BY score_ts
