a1f82d04767a6fb9125900c066257f50afbe2623
Ask Bjørn Hansen <ask@develooper.com>
2022-02-20 16:22:09 -0800

    Use List::Util for uniq()

--- lib/NTPPool/Control.pm
+++ lib/NTPPool/Control.pm
@@ -88,8 +88,8 @@ sub init {
             return $self->redirect($self->_url($self->site, $self->request->path));
         }
         else {
-            # we're setting Strict-Transport-Security with haproxy
-            # $self->request->header_out('Strict-Transport-Security', 'max-age=' . (86400 * 7 * 20));
+           # we're setting Strict-Transport-Security with haproxy
+           # $self->request->header_out('Strict-Transport-Security', 'max-age=' . (86400 * 7 * 20));
         }
     }
 
@@ -110,8 +110,7 @@ sub init {
         return $self->redirect($path, 301);
     }
 
-    $self->tpl_param(
-             'pool_domain' => Combust::Config->new->site->{ntppool}->{pool_domain}
+    $self->tpl_param('pool_domain' => Combust::Config->new->site->{ntppool}->{pool_domain}
           || 'pool.ntp.org');
 
     return OK;
@@ -233,8 +232,7 @@ sub localize_url {
         $uri->path("/$lang" . $uri->path);
         $self->request->header_out('Vary', 'Accept-Language');
         $self->cache_control(
-            's-maxage=900, max-age=3600, stale-while-revalidate=90, stale-if-error=43200'
-        );
+            's-maxage=900, max-age=3600, stale-while-revalidate=90, stale-if-error=43200');
         return $self->redirect($uri->as_string);
     }
     return;
